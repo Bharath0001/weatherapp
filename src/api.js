df338d094730f108704c9d7f38849bb430b63a4e
Bharath0001 <bharathvijay2002@gmail.com>
2025-01-19 21:10:23 +0530

    final commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '7fa88c8de7msh81792c142270588p1c196djsn2b7d07ad3cb0',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "c68ee865d8da3132e15154d385c29920";
